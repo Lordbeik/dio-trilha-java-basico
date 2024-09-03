0a5773556cce346cf40a129bd935e05a1bf196ba
Carlos Antonio <gerrilhas@gmail.com>
2024-09-03 16:06:59 -0300

    Adiciona pasta Calculadora e comentrios

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,28 @@
+/** 
+ * <h1>Calculadora</h1>
+  A Caculadora realiza operações matemáticas entre npumeros inteiros
+<p>
+<b>Note:</b> Leia tentamente a documentação desta classes
+para desfrutar dos recursos oferecidos pelo autor
+
+      @author  Gleyson Sampaio
+      @version 1.0
+      @since  01/01/2022
+*/
+ 
+
+public class Calculadora {
+        /**
+         * 
+         * /Este método é utilizado para somar dois números inteiros
+            
+            @param numeroUm : este é o primeiro prâmetro do método
+            @param numeroDois : este é o segundo parâmetro do método 
+            @return : int o resultado deste método é a soma dos dois números.
+         */
+ 
+
+public int somar( int numeroUm,int numeroDois){
+    return numeroUm + numeroDois;
+    }
+}
---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,27 @@
+public class Comentarios {
+
+    public static void main(String[] args){
+
+        //Olá, eu sou um comentário em uma única linha.
+
+        /*
+         * Olá
+         * Eu sou um cometário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um documentário 
+     * a nível de documentação.
+     * que incrível !!!
+     */
+
+    public void metodo(){
+
+    }
+
+}
